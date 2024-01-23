b10bee18752672c1b55e84251e446021bbd7c0fd
karenhernandez <khernandez0329@gmail.com>
2024-01-22 22:27:36 -0800

    started app.py

--- server/app.py
+++ server/app.py
@@ -8,16 +8,37 @@ from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
-# Add your model imports
 
+# Add your model imports
+from models import Chef, Portfolio, Engagement
 
 # Views go here!
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return '<h1>Project Server</h1>'
+    return "<h1>Project Server</h1>"
 
 
-if __name__ == '__main__':
+class Chefs(Resource):
+    pass
+
+
+class ChefsById(Resource):
+    pass
+
+
+class Portfolios(Resource):
+    pass
+
+
+class Engagements(Resource):
+    pass
+
+
+api.add_resource(Chefs, "/chefs")
+api.add_resource(ChefsById, "/chefs/<int:id>")
+api.add_resource(Portfolios, "/portfolios")
+api.add_resource(Engagements, "/engagements")
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
-
