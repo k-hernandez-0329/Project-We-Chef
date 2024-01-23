815f22838123106b9682319146ac72f4102df93e
karenhernandez <khernandez0329@gmail.com>
2024-01-22 20:39:59 -0800

    completed models and seed

--- server/models.py
+++ server/models.py
@@ -4,3 +4,69 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+
+class Chef(db.Model, SerializerMixin):
+    __tablename__ = "chefs"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    specialty = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    location = db.Column(db.String)
+
+    # Relationships
+    # portfolios = db.relationship(
+    #     "Portfolio", back_populates="chef", cascade="all, delete-orphan"
+    # )
+    engagements = db.relationship(
+        "Engagement", back_populates="chef", cascade="all, delete-orphan"
+    )
+
+    portfolios = association_proxy("portfolio", "engagements")
+    # Serialization Rules
+    serialize_rules = ("-engagements",)
+    # Validation
+
+
+class Portfolio(db.Model, SerializerMixin):
+    __tablename__ = "portfolios"
+
+    id = db.Column(db.Integer, primary_key=True)
+    # chef_id = db.Column(db.Integer, db.ForeignKey("chefs.id"), nullable=False)
+    title = db.Column(db.String)
+    description = db.Column(db.String)
+    image_url = db.Column(db.String)
+    # Relationships
+    # chef = db.relationship(
+    #     "Chef",
+    #     back_populates="portfolios",
+    #     cascade="all, delete-orphan",
+    #     single_parent=True,
+    # )
+    engagements = db.relationship(
+        "Engagement", back_populates="portfolio", cascade="all, delete-orphan"
+    )
+
+    chefs = association_proxy("engagements", "chef")
+    # Serialization Rules
+    serialize_rules = ("-engagements",)
+
+
+class Engagement(db.Model, SerializerMixin):
+    __tablename__ = "engagements"
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment_body = db.Column(db.Text)
+    likes = db.Column(db.Integer)
+
+    chef_id = db.Column(db.Integer, db.ForeignKey("chefs.id"), nullable=False)
+    portfolio_id = db.Column(db.Integer, db.ForeignKey("portfolios.id"), nullable=False)
+
+    # Relationships
+    chef = db.relationship("Chef", back_populates="engagements")
+    portfolio = db.relationship("Portfolio", back_populates="engagements")
+
+    # Serialization Rules
+    serialize_rules = ("-chef", "-portfolio")
--- server/seed.py
+++ server/seed.py
@@ -6,12 +6,104 @@ from random import randint, choice as rc
 # Remote library imports
 from faker import Faker
 
+
 # Local imports
 from app import app
-from models import db
+from models import db, Chef, Portfolio, Engagement
 
-if __name__ == '__main__':
-    fake = Faker()
+
+def create_chefs():
+    chefs = []
+    for _ in range(10):
+        specialty = rc(
+            [
+                "Pastry Chef",
+                "Sous Chef",
+                "Executive Chef",
+                "Line Cook",
+                "Culinary Instructor",
+                "Prep Cook",
+            ]
+        )
+        c = Chef(
+            name=fake.name(),
+            specialty=specialty,
+            bio=fake.paragraph(),
+            location=fake.city(),
+        )
+        chefs.append(c)
+
+    return chefs
+
+
+def create_portfolios(chefs):
+    portfolios = []
+    food_related_titles = [
+        "Delicious Dish",
+        "Tasty Creation",
+        "Gourmet Delight",
+        "Savory Masterpiece",
+        "Flavorful Experience",
+    ]
+    food_related_descriptions = [
+        "A delightful culinary creation",
+        "Savor the taste of perfection",
+        "Experience gourmet excellence",
+        "A journey of flavors",
+    ]
+    for _ in range(randint(1, 5)):
+        title = rc(food_related_titles)
+        description = rc(food_related_descriptions)
+        p = Portfolio(
+            title=title,
+            description=description,
+            image_url=fake.image_url(),
+        )
+        portfolios.append(p)
+
+    return portfolios
+
+
+def create_engagements(chefs, portfolios):
+    engagements = []
+    for _ in range(30):
+        e = Engagement(
+            comment_body=fake.text(),
+            likes=randint(0, 15),
+            chef_id=rc(chefs).id,
+            portfolio_id=rc(portfolios).id,
+        )
+        engagements.append(e)
+
+    return engagements
+
+
+if __name__ == "__main__":
+    fake = Faker("en_US")
+
     with app.app_context():
-        print("Starting seed...")
+        print("Creating tables...")
+        db.create_all()
+
+        print("Clearing db...")
         # Seed code goes here!
+        db.session.query(Chef).delete()
+        db.session.query(Portfolio).delete()
+        db.session.query(Engagement).delete()
+
+        print("Seeding chefs...")
+        chefs = create_chefs()
+        db.session.add_all(chefs)
+        db.session.commit()
+
+        print("Seeding portfolios...")
+        portfolios = create_portfolios(chefs)
+        db.session.add_all(portfolios)
+        db.session.commit()
+
+        print("Seeding engagements...")
+        engagements = create_engagements(chefs, portfolios)
+        db.session.add_all(engagements)
+        db.session.commit()
+
+        print("Done seeding!")
